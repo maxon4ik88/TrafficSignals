91d8c43fdaa08495229313c327132f0b68da274c
MaximShvanov <shvanov.m@yandex.ru>
2020-08-20 14:01:18 +0300

    PreFinal

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
+enum SignalState {
+    case off
+    case red
+    case green
+    case yellow
+}
+
+var signalState: SignalState = .off
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switchButton.setTitle("Start", for: .normal)
+        switchButton.layer.cornerRadius = 30
+        
+        redSignalView.isHidden = true
+        yellowSignalView.isHidden = true
+        greenSignalView.isHidden = true
+        redSignalView.layer.cornerRadius = 80
+        yellowSignalView.layer.cornerRadius = 80
+        greenSignalView.layer.cornerRadius = 80
+        
     }
 
-
+    @IBAction func switchButtonPressed() {
+        switch signalState {
+        case .off:
+            redSignalView.isHidden = false
+            signalState = .red
+        case .red:
+            redSignalView.alpha = 0.2
+            yellowSignalView.isHidden = false
+            yellowSignalView.alpha = 1
+            signalState = .yellow
+        case .yellow:
+            yellowSignalView.alpha = 0.2
+            greenSignalView.isHidden = false
+            greenSignalView.alpha = 1
+            signalState = .green
+        case .green:
+            greenSignalView.alpha = 0.2
+            redSignalView.alpha = 1
+            signalState = .red
+        }
+        
+    }
+    
 }
 
